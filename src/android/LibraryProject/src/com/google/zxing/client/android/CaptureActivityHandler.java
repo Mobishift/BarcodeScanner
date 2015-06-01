c84d930e4a9bfbd52f4c8f53f46de397a9fea176
Zhengz <zhengz@masn.cn>
2015-06-01 19:55:01 +0800

    二维码非法的时候修改

--- src/android/LibraryProject/src/com/google/zxing/client/android/CaptureActivityHandler.java
+++ src/android/LibraryProject/src/com/google/zxing/client/android/CaptureActivityHandler.java
@@ -164,57 +164,9 @@ public final class CaptureActivityHandler extends Handler {
                     restartPreviewAndDecode();
                 }
             });
-//            boolean isCoupon = couponRequest.get(url, new retrofit.Callback<CouponRequest.Coupon>() {
-//                @Override
-//                public void success(CouponRequest.Coupon coupon, Response response) {
-//                    if(coupon != null){
-//                        Date date = null;
-//                        if(coupon.check){
-//                            date = Calendar.getInstance().getTime();
-//                        }else{
-//                            if(coupon.used_at != null){
-//                                SimpleDateFormat simpleDateFormat =  new SimpleDateFormat("yyyy-MM-d'T'HH:mm:ss");
-//                                try{
-//                                    date = simpleDateFormat.parse(coupon.used_at);
-//                                }catch (ParseException ex){
-//
-//                                }
-//                            }
-//                        }
-//                        activity.setCouponView(coupon.parkinglot_coupon_name, coupon.check, date, coupon.origin_price);
-//                    }else{
-//                        Toast.makeText(activity, "发生错误，请重新扫描", Toast.LENGTH_SHORT).show();
-//                    }
-//                    restartPreviewAndDecode();
-//                }
-//
-//                @Override
-//                public void failure(RetrofitError retrofitError) {
-//                    switch (retrofitError.getKind()){
-//                        case NETWORK:
-//                            Toast.makeText(activity, "请检查网络链接", Toast.LENGTH_SHORT).show();
-//                            activity.setCouponText("请检查网络链接");
-//                            break;
-//                        case HTTP:
-//                            int status = retrofitError.getResponse().getStatus();
-//                            if(status == 404){
-//                                Toast.makeText(activity, "该优惠券非本停车场优惠券", Toast.LENGTH_SHORT).show();
-//                                activity.setCouponText("该优惠券非本停车场优惠券");
-//                            }else{
-//                                Toast.makeText(activity, "发生错误:" + status, Toast.LENGTH_SHORT).show();
-//                                activity.setCouponText("发生错误:" + status);
-//                            }
-//                            break;
-//                        case UNEXPECTED:
-//                            Toast.makeText(activity, "未知错误:" + retrofitError.getMessage(), Toast.LENGTH_SHORT).show();
-//                            activity.setCouponText("未知错误:" + retrofitError.getMessage());
-//                            break;
-//                    }
-//                    restartPreviewAndDecode();
-//                }
-//            });
             if(!isCoupon){
                 Toast.makeText(activity, "二维码非法", Toast.LENGTH_SHORT).show();
+                activity.setCouponText("初始化");
                 restartPreviewAndDecode();
             }
         }else{
